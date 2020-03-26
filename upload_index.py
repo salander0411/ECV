70a15db9998f0ac372e200b00993028135414f3f
Lei Chen <41903858+3rd-stone@users.noreply.github.com>
2020-03-26 13:19:14 +0800

    Update upload_index.py

--- upload_index.py
+++ upload_index.py
@@ -6,7 +6,7 @@ Created on Tue Mar 24 23:09:07 2020
 @author: chenlaws
 """
 
-import upload
+import _1_upload
 
 file_path = 'btt1.txt'
 maxThreads = 300
@@ -15,19 +15,19 @@ maxThreads = 300
 with open(file_path,'r') as f:
     all_m3u8_lists = f.read().splitlines()
 
-logger = upload.set_logger()
-logger_404 = upload.set_404logger()
+logger = _1_upload.set_logger()
+logger_404 = _1_upload.set_404logger()
 
-q = upload.queue.Queue(maxThreads)
+q = _1_upload.queue.Queue(maxThreads)
 
 for url in all_m3u8_lists:
-    download = upload.requests.get(url).text
+    download = _1_upload.requests.get(url).text
     start    = download.find('000kb') - 1
     newlink  = download[start:]
     new_url  = url[:url.find('index.m3u8')] + newlink  #'http://video2.youxijian.com:8091/20200305/40AEN3QJ/1000kb/hls/index.m3u8'
     
     q.put(new_url)
-    task = upload.download_and_upload(new_url, q, logger, logger_404)
+    task = _1_upload.download_and_upload(new_url, q, logger, logger_404)
     task.start()
 
 q.join()
