2278b30c01c1c99a28e1be30f31b3eae4f2f7af9
Lei Chen <41903858+3rd-stone@users.noreply.github.com>
2020-03-26 13:18:23 +0800

    Update and rename 4.re-upload-ts.py to _4_re_upload_ts.py

--- _4_re_upload_ts.py
+++ _4_re_upload_ts.py
@@ -6,6 +6,6 @@ Created on Mon Mar 23 14:59:45 2020
 @author: chenlaws
 """
 
-import comparets
+import _3_compare_ts
 
-comparets.upload_again(max_thread = 300)
+_3_compare_ts.upload_again(max_thread = 300)
